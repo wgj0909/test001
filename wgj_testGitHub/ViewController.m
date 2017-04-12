4315aa85dbcb24963feedd2ba633bc407f05178a
wangguanjun <wangguanjun@rongzi.com>
2017-04-12 15:16:58 +0800

    添加一个label

--- wgj_testGitHub/ViewController.m
+++ wgj_testGitHub/ViewController.m
@@ -24,6 +24,13 @@
     [btn setTitle:@"点我" forState:(UIControlStateNormal)];
     [btn setTitleColor:[UIColor whiteColor] forState:(UIControlStateNormal)];
     [self.view addSubview:btn];
+    
+    UILabel *label = [[UILabel alloc] init];
+    label.frame = CGRectMake(100,200, 100, 30);
+    label.text = @"你是谁";
+    label.textColor = [UIColor orangeColor];
+    label.font = [UIFont systemFontOfSize:12];
+    [self.view addSubview:label];
 }
 
 
